1f9484f34e1dfc8416c3953fc259f675993b16f1
#BowBoy <25219011+BowBoyGit@users.noreply.github.com>
2020-11-27 21:37:24 -0500

    Update example_01.py

--- python_examples/example_01.py
+++ python_examples/example_01.py
@@ -1,5 +1,12 @@
-module_name = "abs()"
-description = ""
+
+'''
+The print statement is one of the most used modules.
+Your going to see this a lot.
+The hello world print statement.
+It print a 'string' to the screen
+'''
+
+print('hello world')
 
 
 
