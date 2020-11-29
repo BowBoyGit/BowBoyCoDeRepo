58397449ea89b626dfbf6250814e6000db110e22
#BowBoy <25219011+BowBoyGit@users.noreply.github.com>
2020-11-29 04:50:33 -0500

    Update example_01.py

--- python_examples/example_01.py
+++ python_examples/example_01.py
@@ -1,12 +1,4 @@
 
-'''
-The print statement is one of the most used modules.
-Your going to see this a lot.
-The hello world print statement.
-It print a 'string' to the screen
-'''
-
-print('hello world')
 
 
 
